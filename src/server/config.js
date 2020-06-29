def348c79585a5b957693c680181923e5df7e906
Pratyush Narain <32239181+Pratyush1197@users.noreply.github.com>
2020-06-29 18:14:12 +0530

    Add files via upload

---
+++ src/server/config.js
@@ -0,0 +1,27 @@
+//import Rebase from 're-base';
+//import firebase from 'firebase';
+
+module.exports = {
+    apiKey: "AIzaSyB5iTx2BmJ8yFRsRgjrHldXmumAbjkEQhY",
+    authDomain: "task-7c6bf.firebaseapp.com",
+    databaseURL: "https://task-7c6bf.firebaseio.com",
+    projectId: "task-7c6bf",
+    storageBucket: "task-7c6bf.appspot.com",
+    messagingSenderId: "448464237372",
+    appId: "1:448464237372:web:93591feea89800d303e439",
+    measurementId: "G-YR18BVNW02"
+  };
+  //const base = firebase.initializeApp(firebaseConfig)
+ 
+//var ref = database.ref('tasks');
+
+//ref.on("value", gotData, errData);
+//function gotData(data){
+//console.log(data);} 
+  
+ //function errData(error) {
+   //console.log("Error: " + error);}
+
+
+
+  //export default base 
